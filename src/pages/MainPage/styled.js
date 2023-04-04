3a26bd2662d553b4bcd9a2014542d270784a7f4a
unknown <samidinovaalbina@gmail.com>
2023-04-05 01:47:40 +0600

    изменила стили

--- src/pages/MainPage/styled.js
+++ src/pages/MainPage/styled.js
@@ -4,6 +4,7 @@ export const StyledMainPage = {
   Container: styled.div`
     display: flex;
     flex-grow: 1;
+    width: 100%;
     flex-direction: column;
     align-items: center;
     margin-bottom: 4rem;
